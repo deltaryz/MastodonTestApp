6858d9359d99513d6574da4d01bfe4bcefabb2e3
techniponi <wincam97@gmail.com>
2018-09-06 07:14:59 -0500

    add helper.go for helper functions

--- main.go
+++ main.go
@@ -69,12 +69,3 @@ func main() {
 		fmt.Println(timeline[i])
 	}
 }
-
-// DebugPrint will output caveman debug messages explaining exactly every step being taken in detail.
-// It is recommended to call this function repeatedly for each line of text (so the "DEBUG:" text shows up properly.)
-func DebugPrint(msg string) {
-	// TODO: impl more graceful way to differentiate debug messages without needing to call the func repeatedly
-	if config.DebugEnabled {
-		fmt.Println("DEBUG: " + msg)
-	}
-}
